4274c6fc0236c3f4bbe6f447ab61b4534a82512b
M Talha <talha18513@gmail.com>
2026-05-26 15:41:21 +0500

    update website

---
+++ APEX/config.js
@@ -0,0 +1,10 @@
+const SITE_CONFIG = {
+    siteSlug: "apex",
+    domain: "apex-researchlab.com",
+    realStoreUrl: "https://example.com",
+    utmCampaign: "fw_apex_launch",
+    discountCode: "APEX15",
+    emailSubmitUrl: "https://example.com/submit",
+    countdownEndsAt: "2026-06-19T23:59:59-04:00",
+    contactEmail: "contact@apex-researchlab.com"
+};
