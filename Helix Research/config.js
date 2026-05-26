e0bc56841ec47b15aefcf0a6a8182fe65ff6fd83
M Talha <talha18513@gmail.com>
2026-05-26 22:07:54 +0500

    update website

---
+++ Helix Research/config.js
@@ -0,0 +1,10 @@
+const SITE_CONFIG = {
+    siteSlug: "helix",
+    domain: "helixresearchcompany.com",
+    realStoreUrl: "https://example.com",
+    utmCampaign: "fw_helix_launch",
+    discountCode: "HELIX15",
+    emailSubmitUrl: "https://example.com/submit",
+    countdownEndsAt: "2026-06-19T23:59:59-04:00",
+    contactEmail: "contact@helixresearchcompany.com"
+};
